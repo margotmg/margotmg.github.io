154581efd75205af34c4888a8981a75508fc8109
margotmg <164538414+margotmg@users.noreply.github.com>
2026-02-03 15:25:50 +0000

    Deploying to gh-pages from @ margotmg/margotmg.github.io@4a79e7cebe24d60aa8aff9f6b7c59402b235843c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/cv/cv.pdf";
+            window.location.href = "/assets/cv/MargotMadina_cv.pdf";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
